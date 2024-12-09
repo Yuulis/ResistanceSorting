5e1145c19a9838127e232caf731cbf5809cca73c
Yuulis <yuulis2005@gmail.com>
2024-12-09 18:07:38 +0900

    4人分の結合処理を完了

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,16 +2,24 @@
 
 public class Test : MonoBehaviour
 {
+    public float speed = 0.2f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Hello World!!!");
         Debug.Log("Here is yuulis's scene.");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(Vector3.forward * speed);
 
+        if (transform.position.z >= 30)
+        {
+            Debug.Log("speedが30以上になりました");
+        }
     }
 }
