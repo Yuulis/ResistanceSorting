b9f923fbd2a0a91f7dcff7ebf2e5e15a037e0b9e
Yuulis <yuulis2005@gmail.com>
2024-12-09 17:32:32 +0900

    新しいデバッグログの追加

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -6,6 +6,7 @@ public class Test : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello World!!!");
+        Debug.Log("Here is yuulis's scene.");
     }
 
     // Update is called once per frame
