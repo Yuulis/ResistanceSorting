207a99a6cef029ab3de8b6bfee2867cde71585ba
Yuulis <yuulis2005@gmail.com>
2025-01-03 18:44:13 +0900

    Bandクラスの作成

---
+++ Assets/Scripts/ResistanceSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ResistanceSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject resistancePrefab;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnResistance();
+        }
+    }
+
+    private void SpawnResistance()
+    {
+        GameObject resistanceObj = Instantiate(resistancePrefab, transform.position, Quaternion.identity);
+        Resistance resistance = resistanceObj.GetComponent<Resistance>();
+        resistance.id = Random.Range(0, 1000);
+        resistance.bands.Add((Resistance.Color)Random.Range(0, 11));
+    }
+}
